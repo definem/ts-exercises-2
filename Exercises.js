6dad8c90dcfc38cdce3a3c0a1bf280292e1109b1
definem <mmm748844@gmail.com>
2023-09-24 17:07:07 +0500

    exercise-1 is done

--- Exercises.js
+++ Exercises.js
@@ -1,2 +1,9 @@
 "use strict";
 // Bismillah
+// Exercise 1
+const N = 12345;
+const minutes = Math.floor(N / 60);
+const hours = Math.floor(minutes / 60);
+const remainingMinutes = minutes % 60;
+const seconds = N % 60;
+console.log(`${hours} hours, ${remainingMinutes} minutes, ${seconds} seconds have passed since the beginning of the day.`);
--- Exercises.ts
+++ Exercises.ts
@@ -1,2 +1,13 @@
 // Bismillah
 
+// Exercise 1
+
+const N: number = 12345; 
+const minutes: number = Math.floor(N / 60);
+const hours: number = Math.floor(minutes / 60);
+const remainingMinutes: number = minutes % 60;
+const seconds: number = N % 60;
+
+console.log(
+  `${hours} hours, ${remainingMinutes} minutes, ${seconds} seconds have passed since the beginning of the day.`
+);
