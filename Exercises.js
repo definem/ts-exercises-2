58c6961a4a025becf7023218319f9ad7df5084cf
definem <mmm748844@gmail.com>
2023-09-24 17:39:19 +0500

    exercise 6 is done😸

--- Exercises.js
+++ Exercises.js
@@ -1,47 +1,54 @@
 "use strict";
 // Bismillah
-// Exercise 1
-const N = 12345;
-const minutes = Math.floor(N / 60);
-const hours = Math.floor(minutes / 60);
-const remainingMinutes = minutes % 60;
-const seconds = N % 60;
-console.log(`${hours} hours, ${remainingMinutes} minutes, ${seconds} seconds have passed since the beginning of the day.`);
-// Exercise 2
-const A = 5;
-if (A > 0) {
-    console.log("The number A is positive.");
-}
-else if (A === 0) {
-    console.log("The number A is zero.");
+// // Exercise 1
+// const N: number = 12345; 
+// const minutes: number = Math.floor(N / 60);
+// const hours: number = Math.floor(minutes / 60);
+// const remainingMinutes: number = minutes % 60;
+// const seconds: number = N % 60;
+// console.log(
+//   `${hours} hours, ${remainingMinutes} minutes, ${seconds} seconds have passed since the beginning of the day.`
+// );
+// // Exercise 2
+// const A: number = 5;
+// if (A > 0) {
+//   console.log("The number A is positive.");
+// } else if (A === 0) {
+//   console.log("The number A is zero.");
+// } else {
+//   console.log("The number A is negative.");
+// }
+// // Exercise 3
+// const B: number = 7; 
+// if (B % 2 !== 0) {
+//   console.log("B is an odd number.");
+// } else {
+//   console.log("B is not an odd number.");
+// }
+// // Exercise 4
+// const C: number = 8; 
+// if (C % 2 === 0) {
+//   console.log("C is an even number.");
+// } else {
+//   console.log("C is not an even number.");
+// }
+// // Exercise 5
+// const M: number = 10; 
+// const s: number = 5; 
+// const a: number = 7; 
+// const b: number = 3; 
+// if (a > 0 && b > 0) {
+//   console.log("The sentence is correct.");
+// } else {
+//   console.log("The sentence is incorrect.");
+// }
+// Exercise 6
+const a = 10;
+const b = 7;
+const c = 5;
+if (a > b && b > c) {
+    console.log("The conditions are true.");
 }
 else {
-    console.log("The number A is negative.");
-}
-// Exercise 3
-const B = 7;
-if (B % 2 !== 0) {
-    console.log("B is an odd number.");
-}
-else {
-    console.log("B is not an odd number.");
-}
-// Exercise 4
-const C = 8;
-if (C % 2 === 0) {
-    console.log("C is an even number.");
-}
-else {
-    console.log("C is not an even number.");
-}
-// Exercise 5
-const M = 10;
-const s = 5;
-const a = 7;
-const b = 3;
-if (a > 0 && b > 0) {
-    console.log("The sentence is correct.");
-}
-else {
-    console.log("The sentence is incorrect.");
+    console.log("The conditions are false.");
 }
